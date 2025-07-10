f11af55b9712491acdf912bb1bddfaaa5338f2fb
ridwandr <ridwandr.idn@gmail.com>
2025-07-10 12:20:30 +0700

    initial commit

--- etl_main.py
+++ etl_main.py
@@ -12,6 +12,20 @@ from prefect import flow
 from dotenv import load_dotenv
 load_dotenv()
 
+# === Logging Setup ===
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, f"etl_log_{datetime.now().strftime('%Y%m%d')}.log")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler()
+    ]
+)
+
 @flow(name="ETL Pipeline", log_prints=True)
 def run_pipeline(mode:str='replace') -> None:
     """
